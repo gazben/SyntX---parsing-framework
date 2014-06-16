b6824b15079597b8bf5e3c7a31a7f58f96a61fa9
Nagy Gergely <nagygr@gmail.com>
2014-06-16 14:47:02 +0200

    Documentation improved

--- src/util/parser/alternation.h
+++ src/util/parser/alternation.h
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013, Gergely Nagy
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #ifndef _UTIL_PARSER_ALTERNATION_
 #define _UTIL_PARSER_ALTERNATION_
 
@@ -24,12 +48,14 @@ namespace util {
 				 */
 				alternation(base_rule const &first, base_rule const &second) : first(first.clone()), second(second.clone()) {}
 
-
 				/**
 				 * @copydoc util::parser::base_rule::test
 				 */
-				virtual bool test(base_rule::match_range &context, base_rule::match_range &the_match_range); /* @copydoc util::parser::base_rule */
+				virtual bool test(base_rule::match_range &context, base_rule::match_range &the_match_range);
 
+				/**
+				 * @copydoc util::parser::base_rule::clone
+				 */
 				virtual std::shared_ptr<base_rule> clone() const {
 					return std::shared_ptr<base_rule>(new alternation(*this));
 				}
--- src/util/parser/base_rule.h
+++ src/util/parser/base_rule.h
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013, Gergely Nagy
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #ifndef _UTIL_PARSER_BASERULE_
 #define _UTIL_PARSER_BASERULE_
 
@@ -8,15 +32,22 @@
 
 namespace util {
 	namespace parser {
+		/**
+		 * The base class of the rules.
+		 * The operation to be done when there is a match are defined in this class.
+		 */
 		class base_rule {
 			public:
-				using semantic_action = std::function<void(std::string const &)>;
-				using match_range = std::pair< std::string::const_iterator, std::string::const_iterator >;
+				using semantic_action = std::function<void(std::string const &)>; /**< The function type of semantic actions. */
+				using match_range = std::pair< std::string::const_iterator, std::string::const_iterator >; /**< The type of ranges representing match results and ranges to match. */
 
 			private:
-				semantic_action the_semantic_action;
+				semantic_action the_semantic_action; /**< The semantic action assigned to this rule. */
 
 			public:
+				/**
+				 * Destructor.
+				 */
 				virtual ~base_rule() {}
 
 				/**
@@ -30,12 +61,37 @@ namespace util {
 				 */
 				virtual bool test(match_range &context, match_range &the_match_range) = 0;
 
+				/**
+				 * Contains the operations performed when a rule is tried on the current position.
+				 * The method first tries to apply the rule at the current position using the virtual
+				 * \ref test method. If there is a match, \ref the_match_range is set to represent the
+				 * range consumed by the rule and \ref the_semantic_action is called (if one has been
+				 * assigned to this rule).
+				 * @param context the range to be matched
+				 * @param the_match_range the consumed range
+				 * @return true if there was a match
+				 */
 				bool match(match_range &context, match_range &the_match_range);
 
+				/**
+				 * Assigns a \ref semantic_action to the rule.
+				 * @param a_semantic_action the \ref semantic_action to be assigned to the rule
+				 * @return reference of the rule
+				 */
 				base_rule &operator[](semantic_action const &a_semantic_action);
 
+				/**
+				 * Returns a dynamically allocated copy of this object as base_rule pointer.
+				 * @return a dynamically allocated copy of this object as base_rule pointer
+				 */
 				virtual std::shared_ptr<base_rule> clone() const = 0;
 
+				/**
+				 * Reads the contents of a text file to a std::string.
+				 * @param filename the file to be read
+				 * @param the_contents the std::string to be filled
+				 * @return true if the operation was successful
+				 */
 				static bool file_to_string(std::string const &filename, std::string &the_contents);
 		};
 	}
