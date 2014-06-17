d0d92e7eeca66474f82fe76669b7542ebe0090eb
Nagy Gergely <nagygr@gmail.com>
2014-06-17 13:43:00 +0200

    Documentation improved

--- src/applications/symbolic_expression.h
+++ src/applications/symbolic_expression.h
@@ -31,23 +31,48 @@
 #include <util/parser/parser.h>
 
 namespace applications {
+	/**
+	 * Example grammar demonstrating the use of the SyntX framework realizing a LISP-like <a href="http://en.wikipedia.org/wiki/S-expression">Symbolic Expression</a> parser.
+	 */
 	class symbolic_expression {
 		public:
+			/**
+			 * Helper class representing a node in a tree data structure.
+			 * The S-expression parser builds a parsing tree automatically in which the nodes
+			 * are \ref tree_node objects.
+			 */
 			struct tree_node {
-				std::string value;
-				tree_node *parent;
-				std::vector<tree_node *> children;
+				std::string value;					/**< The value of the node. */
+				tree_node *parent;					/**< The address of the parent node. */
+				std::vector<tree_node *> children;	/**< Conatainer holding the addresses of this node's children. */
 
+				/**
+				 * Constructor.
+				 * @param value the value of this node
+				 * @param parent the address of this node's parent
+				 */
 				tree_node(std::string const &value = "", tree_node *parent = nullptr) :
 					value(value),
 					parent(parent) {}
 
+				/**
+				 * Adds a new child to the node.
+				 * @param value the valu of the new child
+				 * @return the address of the new child
+				 */
 				tree_node *add_child(std::string const &value) {
 					tree_node *new_child = new tree_node(value, this);
 					children.push_back(new_child);
 					return new_child;
 				}
 
+				/**
+				 * Destructor.
+				 * Deletes the children of this node. Their
+				 * destructors will delete their children and
+				 * so on. The tree is recursively freed when the
+				 * root node's destructor is executed.
+				 */
 				~tree_node() {
 					for (tree_node *node : children) {
 						delete node;
@@ -56,14 +81,27 @@ namespace applications {
 			};
 
 		private:
-			tree_node root;
-			util::parser::rule expression;
+			tree_node root;					/**< The root node of the tree. */
+			util::parser::rule expression;	/**< The rule that describes a generic symbolic expression. */
 
 		public:
+			/**
+			 * Constructor.
+			 * Defines and builds the grammar.
+			 */
 			symbolic_expression();
 
+			/**
+			 * Parses a string containing an arbitrarily complex symbolic expression and builds the parsing tree.
+			 * @param text the symbolic expression
+			 * @return true if the parsing was successful
+			 */
 			bool test(std::string const &text);
 
+			/**
+			 * Gets the root of the parsing tree.
+			 * @return the root of the parsing tree
+			 */
 			tree_node &get_root() {return root;}
 	};
 }
